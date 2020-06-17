fd0d11c6745d6bf40c17640d438d6f195c393caf
shontanakamura <63178332+shontanakamura@users.noreply.github.com>
2020-06-17 14:38:45 +0900

    test

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 'use strict';
 
 {
-  const x = 100;
-  console.log(x);
-}
+  const numbers = [1, 2, 5, 8, 10];
 
+  // const evenNumbers = numbers.filter(number =>{
+  //   if(number % 2 === 0){
+  //     return true;
+  //   }else {
+  //     return false;
+  //   }
+  // });
+
+  const evenNumbers = numbers.filter(number =>number % 2 === 0);
+  
+
+  console.log(evenNumbers);
+  }
