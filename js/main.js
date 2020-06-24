b1a8b3cdf2f6351987e7f95addb679ba49fab8f6
shontanakamura <63178332+shontanakamura@users.noreply.github.com>
2020-06-24 11:00:12 +0900

    test

--- js/main.js
+++ js/main.js
@@ -1,17 +1,7 @@
 'use strict';
 
-{
-  function update(){
-    // document.querySelector('h1').textContent = 'changed!';
-    // document.querySelector('#target').textContent = "changed!";
-    // document.getElementById('target').textContent = "changed!";
+  document.querySelector('button').addEventListener('click', ()=>{
     document.getElementById('target').textContent = "changed!";
-    document.querySelectorAll('p').forEach((p, index) =>{
+  
+  });
 
-      p.textContent = `${index}番目のpです！`;
-    });
-    
-  }
-
-  setTimeout(update, 1000);
-}
