04243d38f698c496d36e72915a0325bc00bc1334
shontanakamura <63178332+shontanakamura@users.noreply.github.com>
2020-06-25 14:34:08 +0900

    test

--- js/main.js
+++ js/main.js
@@ -1,19 +1,7 @@
 'use strict';
 
-  document.querySelector('button').addEventListener('click', ()=>{
-
-    const colors = document.querySelectorAll('input');
-    let selectedColor;
-
-    colors.forEach(color =>{
-      if(color.checked === true){
-        selectedColor = color.value;
-      }
-    });
-
-    const li = document.createElement('li');
-    li.textContent = selectedColor;
-    document.querySelector('ul').appendChild(li);
-  });
+  {
+    
+  }
 
 
